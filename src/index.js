6649217826710e917d539fe45791a5db3ec16c67
chenghao <hao.cheng@karakal.com.cn>
2017-10-24 21:33:42 +0800

    :rotating_light: 去掉react-hot-loader错误提示

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ const middleware = [thunk];
 const store = createStore(reducer, applyMiddleware(...middleware));
 console.log(store.getState());
 
+
 const render = Component => {   // 增加react-hot-loader保持状态刷新操作，如果不需要可去掉并把下面注释的打开
     ReactDOM.render(
         <AppContainer>
@@ -33,6 +34,18 @@ render(CRouter);
 
 // Webpack Hot Module Replacement API
 if (module.hot) {
+    // 隐藏You cannot change <Router routes>; it will be ignored 错误提示
+    // react-hot-loader 使用在react-router 3.x上引起的提示，react-router 4.x不存在
+    // 详情可参照https://github.com/gaearon/react-hot-loader/issues/298
+    const orgError = console.error; // eslint-disable-line no-console
+    console.error = (...args) => { // eslint-disable-line no-console
+        if (args && args.length === 1 && typeof args[0] === 'string' && args[0].indexOf('You cannot change <Router routes>;') > -1) {
+            // React route changed
+        } else {
+            // Log the error as normally
+            orgError.apply(console, args);
+        }
+    };
     module.hot.accept('./routes', () => {
         render(CRouter);
     })
