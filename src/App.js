9401b198f90a546b2f3ffe7d72eee0e27c26c43b
chenghao <hao.cheng@karakal.com.cn>
2017-12-12 18:04:09 +0800

    :fire::art: add profile

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Layout } from 'antd';
+import { Layout, notification, Icon } from 'antd';
 import './style/index.less';
 import SiderCustom from './components/SiderCustom';
 import HeaderCustom from './components/HeaderCustom';
@@ -25,6 +25,28 @@ class App extends Component {
             this.getClientWidth();
             // console.log(document.body.clientWidth);
         }
+    }
+    componentDidMount() {
+        const openNotification = () => {
+            notification.open({
+              message: '博主-yezihaohao',
+              description: (
+                  <div>
+                      <p>
+                          GitHub地址： <a href="https://github.com/yezihaohao" target="_blank" rel="noopener noreferrer">https://github.com/yezihaohao</a>
+                      </p>
+                      <p>
+                          博客地址： <a href="https://yezihaohao.github.io/" target="_blank" rel="noopener noreferrer">https://yezihaohao.github.io/</a>
+                      </p>
+                  </div>
+              ),
+              icon: <Icon type="smile-circle" style={{ color: 'red' }} />,
+              duration: 0,
+            });
+            localStorage.setItem('isFirst', JSON.stringify(true));
+        };
+        const isFirst = JSON.parse(localStorage.getItem('isFirst'));
+        !isFirst && openNotification();
     }
     getClientWidth = () => {    // 获取当前浏览器宽度并设置responsive管理响应式
         const { receiveData } = this.props;
