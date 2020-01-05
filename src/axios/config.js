53760d121cb50adc9cff240a649b27d906e5f599
chenghao <hao.cheng@karakal.com.cn>
2020-01-06 01:24:12 +0800

    fix(api-config): replace mock api

---
+++ src/axios/config.js
@@ -0,0 +1,18 @@
+/**
+ * Created by 叶子 on 2017/7/30.
+ * 接口地址配置文件
+ */
+
+//easy-mock模拟数据接口地址
+const MOCK_API = 'https://react-admin-mock.yezihaohao.now.sh/api';
+export const MOCK_AUTH_ADMIN = MOCK_API + '/admin.js'; // 管理员权限接口
+export const MOCK_AUTH_VISITOR = MOCK_API + '/visitor.js'; // 访问权限接口
+
+// github授权
+export const GIT_OAUTH = 'https://github.com/login/oauth';
+// github用户
+export const GIT_USER = 'https://api.github.com/user';
+
+// bbc top news
+export const NEWS_BBC =
+    'https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=429904aa01f54a39a278a406acf50070';
